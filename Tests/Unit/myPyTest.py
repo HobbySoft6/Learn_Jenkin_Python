5685500b81744ef56393d58e99eb93a4cc3781fc
jason feng <jinchunf@yahoo.com>
2020-03-14 17:39:19 -0400

    m

--- Tests/Unit/myPyTest.py
+++ Tests/Unit/myPyTest.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     moduleName = os.path.split(working_dir)[-1]
     anotherDir = "--junitxml={0}".format(os.path.join(working_dir, 'reports', "xunit-result-{0}.xml".format(moduleName)))
     print(anotherDir)
+    print(os.getcwd()+"\Tests")
     print('aaaaaaaaaaaaa')
     testDir = [os.getcwd()+"\Tests", anotherDir]
-
+   
     pytest.main(testDir)
